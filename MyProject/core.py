a19e32ed9ba052540b3f82b8722f0931fd5594a6
Renlong Zheng <renloz@uw.edu>
2018-02-25 23:13:39 -0800

    fix the FileNotFound error from both core.py and test_dataframe

--- MyProject/core.py
+++ MyProject/core.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 def Database():
     """compile data sets into a data library, the output will be a DataFrame"""
-    data_1 = pd.read_excel('data/Flash Point and Cetane Number Predictions for Fuel Compounds.xls', skiprows=3)
+    data_1 = pd.read_excel('../data/Flash Point and Cetane Number Predictions for Fuel Compounds.xls', skiprows=3)
     data = data_1[['Name','Family', 'FP Exp.', 'CN Exp.']]
     result_1 = data.drop(index=0)
     result_1.reset_index(drop=True, inplace=True)
-    data_2 = pd.read_excel('data/Flash Point and Cetane Number Predictions for Fuel Compounds.xls', skiprows=4)
+    data_2 = pd.read_excel('../data/Flash Point and Cetane Number Predictions for Fuel Compounds.xls', skiprows=4)
     result_2 = data_2.loc[: , '-H': 'aaCa']
     result = pd.concat([result_1, result_2], axis=1)
     return result
