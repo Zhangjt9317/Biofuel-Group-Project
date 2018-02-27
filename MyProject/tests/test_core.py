7db2587660da2740cc72d1aa25744c1e7f5f3d9d
Cheng Zeng <zengc7@uw.edu>
2018-02-27 15:24:59 -0800

    Modified test function for database()

--- MyProject/tests/test_core.py
+++ MyProject/tests/test_core.py
@@ -1,5 +1,11 @@
-from mymodule import add
-def test_add(a,b):
-	assert add(1,1)==2
-	assert add(0,1)==1
-	assert add(-1,1)==0
+import sys
+sys.path.append('../..')
+
+import MyProject
+
+
+def test_output_database():
+    '''Check the shape of the database'''
+    result = MyProject.core.Database()
+    assert result.shape == (1159, 32), "The database is not right"
+    return
