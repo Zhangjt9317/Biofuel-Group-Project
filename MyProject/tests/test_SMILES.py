fcc76d089f56ff5076cf1dfca8b6cf2fdee5bc77
Cheng Zeng <zengc7@uw.edu>
2018-03-06 18:19:12 -0800

    Modified and comment test_SMILES

--- MyProject/tests/test_SMILES.py
+++ MyProject/tests/test_SMILES.py
@@ -1,28 +1,15 @@
 import sys
 sys.path.append('../..')
 
-from MyProject import SMILES_generator
-
+import MyProject
 
 def test_SMILES():
     """Check the SMILES function could generate the correct molecular structure,based on input CID number"""
-    result1 = SMILES_generator.SMILES(175854)
-    result2 = SMILES_generator.SMILES(13526104)
-    result3 = SMILES_generator.SMILES(887)
-    result4 = SMILES_generator.SMILES(138219)
-    result5 = SMILES_generator.SMILES(86607438)
-    result6 = SMILES_generator.SMILES(21195085)
+    result1 = MyProject.SMILES(175854)  ##check SMILES structures
+    result2 = MyProject.SMILES(13526104)
+    result3 = MyProject.SMILES(887)
     
-    assert result1 == u'C(C=CCO)O','The chemcials: "(2E)-but-2-ene-1,4-diol" convert wrong!'
-    assert result2 == u'CCCCC(CC)(CO)C(C)O', 'The chemcials: "2-Butyl-2-ethylbutane-1,3-diol" convert wrong'
-    assert result3 == u'CO', 'The chemcial: "methanol" convert wrong'
-    assert result4 == u'C1CC(C1)C2=CC=CC=C2', 'The chemcial: "benzene" convert wrong'
-    assert result5 == u'[HH].C1=CC=C(C=C1)O', 'The chemcial: "phenol" convert wrong'
-    assert result6 == u'CCC=C.CCC=C', 'The chemcial: "but-1-ene" convert wrong'
-    
-    if result1 != "O":
-        print('The unit test for SMILES work well!')
-    else:
-        print('The unit test for SMILES runs wrong!')
-
+    assert result1 == 'C(C=CCO)O', 'The chemcial (2E)-but-2-ene-1,4-diol converts wrong!'
+    assert result2 == 'CCCCC(CC)(CO)C(C)O', 'The chemcial 2-Butyl-2-ethylbutane-1,3-diol converts wrong!'
+    assert result3 == 'CO', 'The chemcial methanol converts wrong'
     return
