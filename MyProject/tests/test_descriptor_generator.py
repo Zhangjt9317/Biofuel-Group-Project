52fd6cdab953330f0f9e9faec027c252f6a61098
Cheng Zeng <zengc7@uw.edu>
2018-03-06 20:36:54 -0800

    Commented and Modified test_descriptor_generator

--- MyProject/tests/test_descriptor_generator.py
+++ MyProject/tests/test_descriptor_generator.py
@@ -1,21 +1,19 @@
-import descriptor_generator
+import sys
+sys.path.append('../..')
 
+import MyProject
 
 def test_descriptor_generator():
-	"""Check for the descriptor_generator could generate the correct number of functional group for 
-different components"""
-    result1 = descriptor_generator.descriptor_generator(887)
-    result2 = descriptor_generator.descriptor_generator(175854)
-    result3 = descriptor_generator.descriptor_generator(13526104)
-    result4 = descriptor_generator.descriptor_generator(138219)
-    result5 = descriptor_generator.descriptor_generator(21195085)
-    result6 = descriptor_generator.descriptor_generator(86607438)
+    """Check descriptor_generator can generate correct numbers of functional groups for 
+    specific compound according to CID"""
+    result1 = MyProject.descriptor_generator(887)  ##test methane
+    result2 = MyProject.descriptor_generator(175854)   ##test 2-Butene-1,4-diol, (2E)-
+    result3 = MyProject.descriptor_generator(61038)   ##test 2-Ethyl-2-butyl-1,3-propanediol
 
-    assert result1 == [4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "The descriptor_generator does not work properly"
-    assert result2 == [8, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "The descriptor_generator does not work properly"
-    assert result3 == [22, 3, 5, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "The descriptor_generator does not work properly"
-    assert result4 == [12, 0, 3, 1, 0, 0, 0, 0, 0, 3, 1, 0, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "The descriptor_generator does not work properly"
-    assert result5 == [16, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "The descriptor_generator does not work properly"
-    assert result6 == [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0], "The descriptor_generator does not work properly"
-
+    assert result1 == [4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], (
+                      "The descriptor_generator does not work properly")
+    assert result2 == [8, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], (
+                      "The descriptor_generator does not work properly")
+    assert result3 == [20, 2, 6, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], (
+                      "The descriptor_generator does not work properly")
     return
