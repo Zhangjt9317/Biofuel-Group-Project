784c83cb4b905e71d3179bf5e074ad1af1f3c218
fduwxcg <xcg27@uw.edu>
2018-03-09 19:16:04 -0800

    change the functions

--- MyProject/clfdata.py
+++ MyProject/clfdata.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn.model_selection 
+import sklearn.model_selection
 from sklearn.model_selection import train_test_split
 from database import Database
 from sklearn.metrics import mean_squared_error
@@ -13,7 +13,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 def data_clean():
     """split data and generate train & test subset"""
     df = Database()
-    train, test = train_test_split(df, test_size=0.1)
+    train, test = train_test_split(df, test_size=0.1, random_state=2)
     a = train.loc[:,'[H]': '[cX3H0](:*)(:*):*']
     X_train = a.mask(a>0, 1)
     y_train = train['Family']
@@ -23,21 +23,22 @@ def data_clean():
     y_test = test['Family']
     return X_train, y_train, X_test, y_test
 
-
 def train_knn(k, X_train, y_train):
     """use knn method to train data"""
     knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train, y_train)
     return knn
 
-def test_knn(k, X_train, y_train, X_test, y_test):
+def test_knn():
     """plot train and test classification result"""
+    X_train, y_train, X_test, y_test = data_clean()
+    k = 5
     knn = train_knn(k, X_train, y_train)
     y_pred = knn.predict(X_test)
     train_pred = knn.predict(X_train)
     print('k =', k)
     print('Accuracy =', metrics.accuracy_score(y_test, y_pred))
-    plt.figure(figsize=(10,8))
+    ax = plt.figure(figsize=(10,8))
     plt.scatter(y_test, y_pred, marker='s', s=100, label='test')
     plt.scatter(y_train, train_pred, marker='d', s=60,c='orange', label='train')
     plt.plot([0,7], [0,7], color='k')
@@ -46,10 +47,12 @@ def test_knn(k, X_train, y_train, X_test, y_test):
     plt.ylabel('Predicted Family', fontsize=15)
     plt.title('Knn Classification',fontsize=20)
     plt.legend()
-    return
+    return ax
 
-def predict_family_knn(k, X_train, y_train, X):
+def predict_family_knn(X):
     """predit family of import molecule X"""
+    X_train, y_train, X_test, y_test = data_clean()
+    k = 5
     knn = train_knn(k, X_train, y_train)
     y_pred = knn.predict(X)
     return y_pred
@@ -59,12 +62,13 @@ def train_lda(X_train, y_train):
     lda.fit(X_train, y_train)
     return lda
 
-def test_lda(X_train, y_train, X_test, y_test):
+def test_lda():
+    X_train, y_train, X_test, y_test = data_clean()
     lda = train_lda(X_train, y_train)
     y_pred = lda.predict(X_test)
-    train_pred = lda.predict(X_train)    
+    train_pred = lda.predict(X_train)
     print('Accuracy = ', metrics.accuracy_score(y_test, y_pred))
-    plt.figure(figsize=(10,8))
+    ax = plt.figure(figsize=(10,8))
     plt.scatter(y_test, y_pred, marker='s', s=100, label='test')
     plt.scatter(y_train, train_pred, marker='d', s=60,c='orange', label='train')
     plt.plot([0,7], [0,7], color='k')
@@ -73,9 +77,10 @@ def test_lda(X_train, y_train, X_test, y_test):
     plt.ylabel('Predicted Family', fontsize=15)
     plt.title('LDA Classification',fontsize=20)
     plt.legend()
-    return
+    return ax
 
-def predict_family_lda(X_train, y_train, X):
+def predict_family_lda(X):
+    X_train, y_train, X_test, y_test = data_clean()
     lda = train_lda(X_train, y_train)
     y_pred = lda.predict(X)
     return y_pred
@@ -85,12 +90,13 @@ def train_svm(X_train, y_train):
     svm.fit(X_train, y_train)
     return svm
 
-def test_svm(X_train, y_train, X_test, y_test):
+def test_svm():
+    X_train, y_train, X_test, y_test = data_clean()
     svm = train_svm(X_train, y_train)
     y_pred = svm.predict(X_test)
     train_pred = svm.predict(X_train)
     print('Accuracy = ', metrics.accuracy_score(y_test, y_pred))
-    plt.figure(figsize=(10,8))
+    ax = plt.figure(figsize=(10,8))
     plt.scatter(y_test, y_pred, marker='s', s=100, label='test')
     plt.scatter(y_train, train_pred, marker='d', s=60,c='orange', label='train')
     plt.plot([0,7], [0,7], color='k')
@@ -99,9 +105,10 @@ def test_svm(X_train, y_train, X_test, y_test):
     plt.xlabel('Actual Family', fontsize=15)
     plt.ylabel('Predicted Family', fontsize=15)
     plt.title('SVM Classification',fontsize=20)
-    return
+    return ax
 
-def predict_family_svm(X_train, y_train, X):
+def predict_family_svm(X):
+    X_train, y_train, X_test, y_test = data_clean()
     svm = train_svm(X_train, y_train)
     y_pred = svm.predict(X)
     return y_pred
