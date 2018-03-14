0b65deec6e048b0cf289eecf92552c1082d86fc8
Jingtian Zhang <jtz9317@gmail.com>
2018-03-14 15:28:39 -0700

    Delete check_input.py

--- MyProject/check_input.py
+++
@@ -1,8 +0,0 @@
-def check_input(CID):
-    """Check the CID number is correct"""
-    if type(CID) != int:    #Check the type of input
-        print('Please check the CID number and insert again :)')
-    elif CID in range(1000000000):   #Check the range of CID number
-        return CID
-    else:
-        print(('Please check you CID number make sure you input the correct CID number'))
